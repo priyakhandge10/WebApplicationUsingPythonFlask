e1d8569b7615733923070b8f1627020a3dcd22a8
priyanka khandge <priyakhandge10@gmail.com>
2021-10-06 17:08:25 +0530

    mysql database connection

--- Hello.py
+++ Hello.py
@@ -1,23 +1,23 @@
 import webbrowser
 from threading import Timer
-from flask import Flask, render_template
+from flask import Flask, render_template, request, make_response
 from flask_mysqldb import MySQL
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder='template')
+
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'ignition8.1goldtest'
+app.config['MYSQL_HOST'] = 'localhost'
+mysql = MySQL(app)
 
 @app.route("/")
-def hello():
-      app.config['MYSQL_HOST'] = 'localhost'
-      app.config['MYSQL_USER'] = 'root'
-      app.config['MYSQL_PASSWORD'] = 'root'
-      app.config['MYSQL_DB'] = 'ignition8.1goldtest'
- 
-      mysql = MySQL(app)
-      cursor = mysql.connection.cursor()
-      cursor.execute(''' SELECT * FROM tasks''')
+def home():
+      cur = mysql.connection.cursor()
+      cur.execute("SELECT * FROM tasks")
       mysql.connection.commit()
-      cursor.close()
-      return render_template('Home.html')
+      data = cur.fetchall()
+      return render_template('home.html', data=data)
 
 def open_browser():
       webbrowser.open_new('http://127.0.0.1:2000/')
